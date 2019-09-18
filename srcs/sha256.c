b57f020a664f74f64154f38e386d3f7153204932
Francois MAURY <fmaury@e2r5p2.42.fr>
2019-09-18 16:28:29 +0200

    norme

--- srcs/sha256.c
+++ srcs/sha256.c
@@ -6,36 +6,46 @@
 /*   By: fmaury <fmaury@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 13:17:30 by fmaury            #+#    #+#             */
-/*   Updated: 2019/09/18 16:03:05 by fmaury           ###   ########.fr       */
+/*   Updated: 2019/09/18 16:27:30 by fmaury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ssl.h>
-//https://github.com/B-Con/crypto-algorithms/blob/master/sha256.c
 
+static const unsigned int g_k[64] = {
+	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
+	0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+	0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
+	0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+	0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+	0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+	0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+	0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+	0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 };
 
-static const unsigned int k[64] = {
-	0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
-	0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
-	0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-	0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
-	0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
-	0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-	0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
-	0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
-};
-
-void sha256_transform(t_ctx *ctx, const unsigned char data[])
+void	sha256_transform(t_ctx *ctx, const unsigned char data[])
 {
-	unsigned int h[8];
-	unsigned int i, j;
-	unsigned int t1, t2, m[64];
+	unsigned int	h[8];
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	t[2];
+	unsigned int	m[64];
 
-	for (i = 0, j = 0; i < 16; ++i, j += 4)
+	i = 0;
+	j = 0;
+	while (i < 16)
+	{
 		m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
-	for ( ; i < 64; ++i)
+		i++;
+		j += 4;
+	}
+	while (i < 64)
+	{
 		m[i] = sig1(m[i - 2]) + m[i - 7] + sig0(m[i - 15]) + m[i - 16];
-
+		i++;
+	}
 	h[0] = ctx->state[0];
 	h[1] = ctx->state[1];
 	h[2] = ctx->state[2];
@@ -44,20 +54,21 @@ void sha256_transform(t_ctx *ctx, const unsigned char data[])
 	h[5] = ctx->state[5];
 	h[6] = ctx->state[6];
 	h[7] = ctx->state[7];
-
-	for (i = 0; i < 64; ++i) {
-		t1 = h[7] + ep1(h[4]) + ch(h[4],h[5],h[6]) + k[i] + m[i];
-		t2 = ep0(h[0]) + maj(h[0],h[1],h[2]);
+	i = 0;
+	while (i < 64)
+	{
+		t[0] = h[7] + ep1(h[4]) + ch(h[4], h[5], h[6]) + g_k[i] + m[i];
+		t[1] = ep0(h[0]) + maj(h[0], h[1], h[2]);
 		h[7] = h[6];
 		h[6] = h[5];
 		h[5] = h[4];
-		h[4] = h[3] + t1;
+		h[4] = h[3] + t[0];
 		h[3] = h[2];
 		h[2] = h[1];
 		h[1] = h[0];
-		h[0] = t1 + t2;
+		h[0] = t[0] + t[1];
+		i++;
 	}
-
 	ctx->state[0] += h[0];
 	ctx->state[1] += h[1];
 	ctx->state[2] += h[2];
@@ -68,7 +79,7 @@ void sha256_transform(t_ctx *ctx, const unsigned char data[])
 	ctx->state[7] += h[7];
 }
 
-void sha256_init(t_ctx *ctx)
+void	sha256_init(t_ctx *ctx)
 {
 	ctx->datalen = 0;
 	ctx->bitlen = 0;
@@ -82,32 +93,38 @@ void sha256_init(t_ctx *ctx)
 	ctx->state[7] = 0x5be0cd19;
 }
 
-void sha256_update(t_ctx *ctx, const unsigned char data[], size_t len)
+void	sha256_update(t_ctx *ctx, const unsigned char data[], size_t len)
 {
 	unsigned int i;
 
-	for (i = 0; i < len; ++i) {
+	i = 0;
+	while (i < len)
+	{
 		ctx->data[ctx->datalen] = data[i];
 		ctx->datalen++;
-		if (ctx->datalen == 64) {
+		if (ctx->datalen == 64)
+		{
 			sha256_transform(ctx, ctx->data);
 			ctx->bitlen += 512;
 			ctx->datalen = 0;
 		}
+		i++;
 	}
 }
 
-void sha256_final(t_ctx *ctx, unsigned char hash[])
+void	sha256_final(t_ctx *ctx, unsigned char hash[])
 {
 	unsigned int i;
 
 	i = ctx->datalen;
-	if (ctx->datalen < 56) {
+	if (ctx->datalen < 56)
+	{
 		ctx->data[i++] = 0x80;
 		while (i < 56)
 			ctx->data[i++] = 0x00;
 	}
-	else {
+	else
+	{
 		ctx->data[i++] = 0x80;
 		while (i < 64)
 			ctx->data[i++] = 0x00;
@@ -124,29 +141,30 @@ void sha256_final(t_ctx *ctx, unsigned char hash[])
 	ctx->data[57] = ctx->bitlen >> 48;
 	ctx->data[56] = ctx->bitlen >> 56;
 	sha256_transform(ctx, ctx->data);
-	for (i = 0; i < 4; ++i) {
-		hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
+	i = 0;
+	while (i < 4)
+	{
+		hash[i] = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 4] = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 8] = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
 		hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
 		hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
 		hash[i + 20] = (ctx->state[5] >> (24 - i * 8)) & 0x000000ff;
 		hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
 		hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
+		i++;
 	}
 }
 
 int		sha256(t_ssl *ssl)
 {
-	t_ctx ctx;
+	t_ctx			ctx;
 	unsigned char	buf[32];
 
 	sha256_init(&ctx);
-	printf("%s %zu\n",(char*)ssl->plain, ssl->size);
-
 	sha256_update(&ctx, ssl->plain, ssl->size);
 	sha256_final(&ctx, buf);
-		for (int i = 0; i < 32; i++)
+	for (int i = 0; i < 32; i++)
 		printf("%2.2x", buf[i]);
 	puts("");
 	return (1);
